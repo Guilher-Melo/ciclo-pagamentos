8873ffcd9676c9b82d3ad95ed2467cda47b90d47
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2023-06-13 14:46:10 -0300

    first commit

---
+++ src/main/app.jsx
@@ -0,0 +1,12 @@
+import '../common/templates/dependecies';
+import React from "react";
+
+import Header from '../common/templates/header';
+
+export default function App(props) {
+  return(
+    <div className="wrapper">
+      <Header />
+    </div>
+  )
+}
