a40246482c1719ac33b35f8c5347be87a9b7740c
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2023-06-14 19:04:22 -0300

    Construindo dashboard com redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM  from "react-dom";
-
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 import App from "./main/App";
+import reducers from './main/reducers';
+
+import promise from "redux-promise";
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('app')
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  ,document.getElementById('app')
 )
