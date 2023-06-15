d89d7da64b5e0ffce39c1296e04b6654d62495cd
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2023-06-15 15:43:16 -0300

    Criando componente tab

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import ContentHeader from '../common/templates/contentHeader';
+import Content from '../common/templates/content';
+import Tabs from "../common/tab/tabs";
+import TabsContent from "../common/tab/tabsContent";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabHeader from "../common/tab/tabHeader";
 
-export default function BillingCycle(props) {
-  return(
+export default class BillingCycle extends Component {
+  render() {
+    return (
     <div>
-      <h1>Ciclo de pagamentos</h1>
+      <ContentHeader title="Ciclo de Pagamentos" small="Cadastro"/>
+      <Content>
+        <Tabs>
+          <TabsHeader>
+            <TabHeader label="Listar" icon="bars" target='tabList' />
+            <TabHeader label="Incluir" icon="plus" target='tabCreate' />
+            <TabHeader label="Alterar" icon="pencil" target='tabUpdate' />
+            <TabHeader label="Excluir" icon="trash-o" target='tabDelete' />
+          </TabsHeader>
+
+          <TabsContent>
+
+          </TabsContent>
+        </Tabs>
+
+      </Content>
     </div>
-  )
+    )
+  }
+  
 }
